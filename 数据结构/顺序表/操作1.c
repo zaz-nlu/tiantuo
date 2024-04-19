4deab76164c780fc8f6f8ed52aee12c833ece1d0
zaz-nlu <48232287+zaz-nlu@users.noreply.github.com>
2024-04-19 09:15:51 +0800

    Update 操作1.c

--- 数据结构/顺序表/操作1.c
+++ 数据结构/顺序表/操作1.c
@@ -1,4 +1,3 @@
-//想实现的操作就是插入，删除，判满
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,58 +6,62 @@
 
 typedef int datatype_t;
 
-typedef struct{
+typedef struct {
     int id;
     char name[20];
     int score;
-}STU;
+} STU;
 
-typedef struct{
-    datatype_t buf[MAX];//定义数组存储数据
-    STU n;//数组中有效元素个数
-}seqlist_t;
+typedef struct {
+    datatype_t buf[MAX]; // 定义数组存储数据
+    int n;               // 数组中有效元素个数
+} seqlist_t;
 
-seqlist_t *create_empty_list(){
-    seqlist_t* l = (seqlist_t *) malloc(sizeof(seqlist_t))
-    if(NULL = l){
-        printf("Malloc error");
-        return;
+seqlist_t *create_empty_list() {
+    seqlist_t* l = (seqlist_t *) malloc(sizeof(seqlist_t));
+    if (l == NULL) {
+        printf("Malloc error\n");
+        return NULL;
     }
-    memset(l,0,siezof(seqlist_t));
+    memset(l, 0, sizeof(seqlist_t));
     return l;
 }
 
-void  *insert(seqlist_t *l,datatype_t data){
-    l->buf[l->n] = data;
-    l->n++;
-    return;
+void insert(seqlist_t *l, datatype_t data) {
+    if (l->n < MAX) {
+        l->buf[l->n] = data;
+        l->n++;
+    } else {
+        printf("List is full\n");
+    }
 }
 
-void printf_seqlist(seqlist_t *l){
-    int i;
-    for(i=0;i<l->buf[l->n];i++){
-        printf("%d",l->buf[i]);
-
+void print_seqlist(seqlist_t *l) {
+    for (int i = 0; i < l->n; i++) {
+        printf("%d ", l->buf[i]);
     }
     printf("\n");
 }
 
-int is_full_seqlist(seqlist_t *l){
-    return l->n == MAX ?1:0;
+int is_full_seqlist(seqlist_t *l) {
+    return l->n == MAX;
 }
 
-
-
-int main(){
-    seqlist_t *l =create_empty_list();
+int main() {
+    seqlist_t *l = create_empty_list();
+    if (l == NULL) return -1;
+    
     datatype_t data;
-    printf("Please enter number:\n");
-    while(!is_full_seqlist){
-    scanf('%d',&data);
-    insert(l,data);
+    printf("Please enter numbers (up to 10):\n");
+    while (!is_full_seqlist(l)) {
+        if (scanf("%d", &data) != 1) {
+            printf("Invalid input\n");
+            break;
+        }
+        insert(l, data);
     }
-    printf_seqlist(l);
+    print_seqlist(l);
     free(l);
     l = NULL;
-    return 0
+    return 0;
 }
